5c82594e039de770d08cd7fa77aa2a8fbf085c8b
bozer <gamerpig197@gmail.com>
2023-05-01 14:53:06 +0700

    lab5

---
+++ src/bytestreams2/lab5.java
@@ -0,0 +1,37 @@
+package bytestreams2;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author dol
+ */
+public class lab5 {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Student s = new Student("65130500046", "Phatcharadol Thangplubs");
+        try {
+            FileOutputStream file = new FileOutputStream("obj.dat");
+            ObjectOutputStream obj = new ObjectOutputStream(file);
+            obj.writeObject(s);
+        } catch (IOException ex) {
+            Logger.getLogger(lab5.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            FileInputStream file = new FileInputStream("obj.dat");
+            ObjectInputStream input = new ObjectInputStream(file);
+            Student ss = (Student)input.readObject();
+            System.out.println(ss);
+        } catch (IOException ex) {
+            Logger.getLogger(lab5.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
